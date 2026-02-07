d099c15c7d4a64b382e121f2527b944035d934f1
william <descoteaux.william@gmail.com>
2026-02-07 10:46:54 -0500

    pull commit

--- ApiQuiz.Logic/obj/Debug/net10.0/ApiQuiz.Logic.AssemblyInfo.cs
+++ ApiQuiz.Logic/obj/Debug/net10.0/ApiQuiz.Logic.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("ApiQuiz.Logic")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+36180541d9402401339b9f2aec5cefebf20a983b")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+31b1a90708f8360b79b61141b3f292c8d5976da6")]
 [assembly: System.Reflection.AssemblyProductAttribute("ApiQuiz.Logic")]
 [assembly: System.Reflection.AssemblyTitleAttribute("ApiQuiz.Logic")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
